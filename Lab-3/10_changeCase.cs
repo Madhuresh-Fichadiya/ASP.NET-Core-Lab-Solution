9184653c1272510f7f9dda72a3819ad0dec61b6b
sejalgupta001 <sejal.gupta@darshan.ac.in>
2025-07-01 10:23:00 +0530

    Delete Lab-3 directory

--- Lab-3/10_changeCase.cs
+++
@@ -1,6 +0,0 @@
-﻿
-Console.Write("Enter Character : ");
-Char c = Convert.ToChar(Console.ReadLine());
-
-Char newChar = c.ToString().ToUpper().ToCharArray()[0];
-Console.WriteLine("Changed Character : " + newChar);
